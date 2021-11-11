3465846c9b7bb5ab0ddf54601d8ecfd388ca1d1d
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-11-11 00:49:50 -0500

    Add files via upload
    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print ('Welcome to the Vacation Budget Planner App!')
+
+name = input("Please enter your name: ")
+
+destination = input(f"Hi {name} where would you like to travel: Cuba or Canada? ")
+
+print (f"Thanks {name} let's plan a trip to {destination}!")
+
