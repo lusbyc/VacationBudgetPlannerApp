9638ebad92b004c9d1761e569cd69d697bf679a0
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-11-22 14:23:14 -0500

    Week 2 commit
    Added variables and calculations for money spent per day.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ print ('Welcome to the Vacation Budget Planner App!')
 
 name = input("Please enter your name: ")
 
-destination = input(f"Hi {name} where would you like to travel: Cuba or Canada? ")
+destination = input(f"Hi {name}, where would you like to travel? ")
 
-print (f"Thanks {name} let's plan a trip to {destination}!")
+days = int(input(f"That sounds like fun. How many days will you be in {destination}? "))
 
+money = float(input(f"Thanks for the info {name}. How much money will you bring on the trip? "))
+moneyFormatted = format(money,".2f") #I've been trying to avoid doing this as a separate step but I get errors otherwise
+
+moneyPerDay = format(money / days,".2f")
+
+print(f"Hey {name}, you are going to {destination} for {days} days with ${moneyFormatted} for spending money. You can spend ${moneyPerDay} per day.")
