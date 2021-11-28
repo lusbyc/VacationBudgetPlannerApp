085dc7d29435e492a459db331004453cd9dacd9c
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-11-28 10:25:34 -0500

    Added switch statement, while loop, logic, etc
    Added switch statement for destination choice and while loop to allow user to repeat program. Also added formatting for currency and numbers. Called switch statement function again when user makes an invalid selection.

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
+keepGoing = "y"
 
-print ('Welcome to the Vacation Budget Planner App!')
+while keepGoing.upper() == "Y":
+    print()
+    print ('Welcome to the Vacation Budget Planner!')
 
-name = input("Please enter your name: ")
+    name = input("What's your name? ")
 
-destination = input(f"Hi {name}, where would you like to travel? ")
+    money = int(input(f"Hi {name}, what's your budget for the trip? "))
 
-days = int(input(f"That sounds like fun. How many days will you be in {destination}? "))
+    days = int(input(f"How many days do you plan to vacation? "))
 
-money = float(input(f"Thanks for the info {name}. How much money will you bring on the trip? "))
-moneyFormatted = format(money,".2f") #I've been trying to avoid doing this as a separate step but I get errors otherwise
+    def convertUserOption():
+        userInput = int(input("""
+Where would you like to travel? 
 
-moneyPerDay = format(money / days,".2f")
+Choose 
+(1) Mexico
+(2) Jamaica
 
-print(f"Hey {name}, you are going to {destination} for {days} days with ${moneyFormatted} for spending money. You can spend ${moneyPerDay} per day.")
+"""))
+        if userInput == 1:
+            global destination 
+            global currencyName
+            global currencyFormatted
+            global currencyPerDay
+            destination = "Mexico"
+            currencyName = "pesos"
+            currencyFormatted = "₱{:,.2f}".format(money * 21.92)
+            currencyPerDay = "₱{:,.2f}".format((money * 21.92) / days)
+
+        elif userInput == 2: 
+            destination = "Jamaica"
+            currencyName = "Jamaican Dollars"
+            currencyFormatted = "J${:,.2f}".format(money * 154.60)
+            currencyPerDay = "J${:,.2f}".format((money * 154.60) / days)
+
+        else:   
+            print()
+            print("Invalid option. You must select one of the options provided.")
+            convertUserOption()
+        
+    convertUserOption()
+    print()  
+    print('Great,',destination,'sounds like an amazing trip!')
+    print("**********")
+
+    moneyFormatted = "${:,.2f}".format(money)
+    moneyPerDay = "${:,.2f}".format(money / days)
+
+    totalHours = days * 24
+    totalHoursF = "{:,}".format(totalHours)
+    totalMinutes = "{:,}".format(totalHours * 60)
+
+    print(f"You're going to be in {destination} for {days} days. Put another way, you'll be there for {totalHoursF} hours or {totalMinutes} minutes.") 
+
+    print(f"You have {moneyFormatted} in USD for spending money. You can spend {moneyPerDay} per day.")
+    print(f"If you convert this to {currencyName} you'll have {currencyFormatted} to spend for the trip. This is {currencyPerDay} per day.")
+    print()
+    print(f"Enjoy your trip {name}!")
+    
+    keepGoing = input("""
+Would you like to use the Vacation Budget Planner again? 
+Enter 'Y' for Yes or 'N' for No: """).upper()
+    
+print("""
+Thanks for using the Vacation Budget Planner!""")
