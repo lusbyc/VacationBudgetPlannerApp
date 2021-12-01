e3836c95772acd945fc5c795b248558f35baafa5
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-11-30 23:59:06 -0500

    added password functionality

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-def convertUserOption():
-    userInput = int(input("""
-Where would you like to travel? 
-
-Choose 
-(1) Mexico
-(2) Jamaica
-
-"""))
+def switch(userInput):
     if userInput == 1:
         global destination 
+        global money
         global currencyName
         global currencyFormatted
         global currencyPerDay
@@ -16,8 +9,9 @@ Choose
         currencyName = "pesos"
         currencyFormatted = "₱{:,.2f}".format(money * 21.92)
         currencyPerDay = "₱{:,.2f}".format((money * 21.92) / days)
-
+        
     elif userInput == 2: 
+        money 
         destination = "Jamaica"
         currencyName = "Jamaican Dollars"
         currencyFormatted = "J${:,.2f}".format(money * 154.60)
@@ -26,42 +20,66 @@ Choose
     else:   
         print()
         print("Invalid option. You must select one of the options provided.")
-        convertUserOption()
+        quit() #This is acting as a break
+        #convertUserInput() Ideally this method would be called again so that the user can try again
 
-keepGoing = "y"
+attempts = 0
+password = "secret"
 
-while keepGoing.upper() == "Y":
-    print()
-    print ('Welcome to the Vacation Budget Planner!')
-
-    name = input("What's your name? ")
-
-    money = int(input(f"Hi {name}, what's your budget for the trip? "))
-
-    days = int(input(f"How many days do you plan to vacation? "))
+while attempts < 3:
+    attempts += 1
+    loginCredentials = input("Please enter your password: ")
+    if loginCredentials == password:
         
-    convertUserOption()
-    print()  
-    print('Great,',destination,'sounds like an amazing trip!')
-    print("**********")
+        keepGoing = "y"
 
-    moneyFormatted = "${:,.2f}".format(money)
-    moneyPerDay = "${:,.2f}".format(money / days)
+        while keepGoing.upper() == "Y":
+            print()
+            print ('Welcome to the Vacation Budget Planner!')
 
-    totalHours = days * 24
-    totalHoursF = "{:,}".format(totalHours)
-    totalMinutes = "{:,}".format(totalHours * 60)
-
-    print(f"You're going to be in {destination} for {days} days. Put another way, you'll be there for {totalHoursF} hours or {totalMinutes} minutes.") 
-
-    print(f"You have {moneyFormatted} in USD for spending money. You can spend {moneyPerDay} per day.")
-    print(f"If you convert this to {currencyName} you'll have {currencyFormatted} to spend for the trip. This is {currencyPerDay} per day.")
-    print()
-    print(f"Enjoy your trip {name}!")
+            name = input("What's your name? ")
     
-    keepGoing = input("""
-Would you like to use the Vacation Budget Planner again? 
+            print()
+            print(f"Welcome {name}, where would you like to go? ")
+            userInput = int(input("""
+Choose 
+(1) Mexico
+(2) Jamaica
+
+"""))
+            print()
+            money = int(input("What's your budget for the trip? "))
+            print()
+            days = int(input("How many days do you plan to spend on this trip? "))
+        
+            switch(userInput)
+            print()  
+            print('Great,',destination,'sounds like an amazing trip!')
+            print()
+            print("**********")
+            print()
+
+            moneyFormatted = "${:,.2f}".format(money)
+            moneyPerDay = "${:,.2f}".format(money / days)
+
+            totalHours = days * 24
+            totalHoursF = "{:,}".format(totalHours)
+            totalMinutes = "{:,}".format(totalHours * 60)
+
+            print(f"You're going to be in {destination} for {days} days. Put another way, you'll be there for {5totalHoursF} hours or {totalMinutes} minutes.") 
+
+            print(f"You have {moneyFormatted} in USD for spending money. You can spend {moneyPerDay} per day.")
+            print()
+            print(f"If you convert this to {currencyName} you'll have {currencyFormatted} to spend for the trip. This is {currencyPerDay} per day.")
+            print()
+            print(f"Enjoy your trip {name}!")
+    
+            keepGoing = input("""
+Would you like to use th5e Vacation Budget Planner again? 
 Enter 'Y' for Yes or 'N' for No: """).upper()
     
-print("""
-Thanks for using the Vacation Budget Planner!""")
+            print("""
+Thanks for using the Vacation Budget Planner!""")   
+            break
+else:
+    print("You've exceeded the maximum number of attempts.")
