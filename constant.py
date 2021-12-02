023da211ebe522b571e5b53e1e046ce3a21f2cf7
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-02 13:24:55 -0500

    fixed comments

--- constant.py
+++ constant.py
@@ -5,4 +5,4 @@ MEXICAN_EXCHANGE_RATE = 21.92
 
 #Neither the Jamaican or Mexican Exchange Rates should be hard coded as 
 #exchange rates change all the time. They are set as constants here to 
-#adhere to the instructions that the hard coded values be changed to constans.
+#adhere to the instructions that the hard coded values be changed to constants.
