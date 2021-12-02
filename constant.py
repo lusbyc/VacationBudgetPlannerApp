1f25784a8ff39b022708a8ea4ceacd208d38f51c
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-02 12:53:45 -0500

    initial commit

---
+++ constant.py
@@ -0,0 +1,8 @@
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+JAMAICAN_EXCHANGE_RATE = 154.60
+MEXICAN_EXCHANGE_RATE = 21.92
+
+#Neither the Jamaican or Mexican Exchange Rates should be hard coded as 
+#exchange rates change all the time. #They are set as constants here to 
+#adhere to the instructions that the hard coded values be changed to constans.
