70d0cde05ac429958a7b83ede29e74baa4dd025f
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 04:07:09 -0500

    imported pconst library, corrected logic, etc
    imported pconst library to store constant values, corrected comparison statement used to check validity of user input, and changed camel case to snake case.

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
-import constant
+from pconst import const
 
 def switch(userInput):
-    if userInput == 1:
+    if userInput == "1":
         global destination 
         global money
-        global currencyName
-        global currencyFormatted
-        global currencyPerDay
+        global currency_name
+        global currency_formatted
+        global currency_per_day
+        const.MEXICAN_EXCHANGE_RATE = 154.60
         destination = "Mexico"
-        currencyName = "pesos"
-        currencyFormatted = "₱{:,.2f}".format(money * constant.MEXICAN_EXCHANGE_RATE)
-        currencyPerDay = "₱{:,.2f}".format((money * constant.MEXICAN_EXCHANGE_RATE) / days)
+        currency_name = "pesos"
+        currency_formatted = "₱{:,.2f}".format(money * const.MEXICAN_EXCHANGE_RATE)
+        currency_per_day = "₱{:,.2f}".format((money * const.MEXICAN_EXCHANGE_RATE) / days)
         
-    elif userInput == 2: 
+    elif userInput == "2": 
         money 
         destination = "Jamaica"
-        currencyName = "Jamaican Dollars"
-        currencyFormatted = "J${:,.2f}".format(money * constant.JAMAICAN_EXCHANGE_RATE)
-        JAMAICAN_EXCHANGE_RATE = 100 #This line attempts to change the constant value. When program is run line 76 shows that the value wasn't changed.
-        currencyPerDay = "J${:,.2f}".format((money * constant.JAMAICAN_EXCHANGE_RATE) / days)
+        currency_name = "Jamaican Dollars"
+        const.JAMAICAN_EXCHANGE_RATE = 21.92
+        currency_formatted = "J${:,.2f}".format(money * const.JAMAICAN_EXCHANGE_RATE)
+        currency_per_day = "J${:,.2f}".format((money * const.JAMAICAN_EXCHANGE_RATE) / days)
 
-    else:   
-        print()
-        print("Invalid option. You must select one of the options provided.")
-        quit() #This ends the program. Alternately a loop can be utilized to give the user another chance to enter a valid option.
+    # else:   
+    #     print()
+    #     print("Invalid option. You must select one of the options provided.")
+    #     quit() #This ends the program. Alternately a loop can be utilized to give the user another chance to enter a valid option.
+
+const.MINUTES_PER_HOUR = 60
+const.HOURS_PER_DAY = 24
 
 attempts = 0
 password = "secret"
 
 while attempts < 3:
     attempts += 1
-    loginCredentials = input("Please enter your password: ")
-    if loginCredentials == password:
+    login_credentials = input("Please enter your password: ")
+    if login_credentials == password:
         
-        keepGoing = "y"
+        keep_going = "y"
 
-        while keepGoing.upper() == "Y":
+        while keep_going.upper() == "Y":
             print()
             print ('Welcome to the Vacation Budget Planner!')
 
@@ -43,38 +47,45 @@ while attempts < 3:
     
             print()
             print(f"Welcome {name}, where would you like to go? ")
-            userInput = int(input("""
+            user_input = input("""
 Choose 
 (1) Mexico
 (2) Jamaica
 
-"""))
-            print()
-            money = int(input("What's your budget for the trip? "))
-            print()
-            days = int(input("How many days do you plan to spend on this trip? "))
-        
-            switch(userInput)
+""")
+            if user_input != "1" and user_input != "2":
+                print()
+                print("Invalid option. You must select one of the options provided.")
+                quit() #This ends the program. Alternately a loop can be utilized to give the user another chance to enter a valid option.
+            
+            else:
+                print()
+                money = int(input("What's your budget for the trip in US dollars? "))
+                print()
+                days = int(input("How many days do you plan to spend on this trip? "))
+
+                switch(user_input)
+
             print()  
-            print('Great,',destination,'sounds like an amazing trip!')
+            print(f"Great, {destination} sounds like an amazing trip!")
             print()
             print("**********")
 
-            moneyFormatted = "${:,.2f}".format(money)
-            moneyPerDay = "${:,.2f}".format(money / days)
+            money_formatted = "${:,.2f}".format(money)
+            money_per_day = "${:,.2f}".format(money / days)
 
-            totalHours = days * constant.MINUTES_PER_HOUR
-            totalHoursF = "{:,}".format(totalHours)
-            totalMinutes = "{:,}".format(totalHours * constant.HOURS_PER_DAY)
+            total_hours = days * const.HOURS_PER_DAY
+            total_hours_formatted = "{:,}".format(total_hours)
+            total_minutes = "{:,}".format(total_hours * const.MINUTES_PER_HOUR)
 
             print(f"""
-You're going to be in {destination} for {days} days. Put another way, you'll be there for {totalHoursF} hours or {totalMinutes} minutes.
-You have {moneyFormatted} in USD for spending money. You can spend {moneyPerDay} per day. If you convert this to {currencyName} 
-you'll have {currencyFormatted} to spend for the trip. This is {currencyPerDay} per day.
+You're going to be in {destination} for {days} days. Put another way, you'll be there for {total_hours_formatted} hours or {total_minutes} minutes.
+You have {money_formatted} in US dollars for spending money. You can spend {money_per_day} per day. If you convert this to {currency_name} 
+you'll have {currency_formatted} to spend for the trip. This is {currency_per_day} per day.
             
 Enjoy your trip {name}!""")
     
-            keepGoing = input("""
+            keep_going = input("""
 Would you like to use the Vacation Budget Planner again? 
 Enter 'Y' for Yes or 'N' for No: """).upper()
         print("""
